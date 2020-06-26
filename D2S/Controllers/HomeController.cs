002206a5d9a45cc0d535f5a66eaae2a4ac85235f
Stefan Barbalata <3428.stefan.barbalata@eped.pt>
2020-06-26 18:46:05 +0100

    FUCK OFF

--- D2S/Controllers/HomeController.cs
+++ D2S/Controllers/HomeController.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using D2S.Models;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
 
 namespace D2S.Controllers
 {
     public class HomeController : Controller
     {
+        LoginDB db = new LoginDB();
+
         public IActionResult Index()
         {
             return View("../Home/PT/index");
+        }
+
+        [HttpPost]
+        public IActionResult Index([Bind] Login log)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    string res = db.Save(log);
+                    if (res == "s")
+                        return RedirectToAction("index", "Loged", TempData["nome"] = log.email);
+                    if (res == "n")
+                        return RedirectToAction("index22", "home");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["msg"] = ex.Message;
+            }
+            return RedirectToAction("index", "home");
         }
 
         public IActionResult Sobre()
---
+++ D2S/Models/LoginDB.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D2S.Models
+{
+    public class LoginDB
+    {
+
+        MySqlConnection con = new MySqlConnection("server=remotemysql.com;user id=DNy7ZqW3WA;persistsecurityinfo=True;database=DNy7ZqW3WA; password=2abgrxDSfR");
+        public string Save(Login log)
+        {
+            string dd = "";
+            try
+            {
+                MySqlCommand com = new MySqlCommand("SELECT * FROM clientes WHERE email='" + log.email + "' AND pass='" + log.pass + "'", con);
+                MySqlCommand com1 = new MySqlCommand("SELECT verificar FROM clientes WHERE email='" + log.email + "' AND pass='" + log.pass + "'", con);
+                con.Open();
+                MySqlDataReader dr1 = com1.ExecuteReader();
+                if (dr1.Read())
+                {
+                    dd = dr1.GetString(0);
+                }
+                dr1.Dispose();
+                string dd1 = "";
+                if (dd == "")
+                {
+                    dd1 = "s";
+                }
+                else
+                    dd1 = "n";
+
+                MySqlDataReader dr = com.ExecuteReader();
+                if (dr.Read())
+                {
+                    con.Close();
+                    return (dd1);
+                }
+                else
+                {
+                    return ("E-mail ou Palavra-Passe incorretas.");
+                }
+            }
+            catch (Exception ex)
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+                return (ex.Message.ToString());
+
+            }
+        }
+    }
+}
